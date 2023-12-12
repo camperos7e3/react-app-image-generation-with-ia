3d0ba0d9d6e497f6eb000e012886cfe62277c819
camperos7e3 <camperos.7e3@proton.me>
2023-12-12 15:37:31 +0100

    update dependencies

--- backend/routes/dalleRoutes.js
+++ backend/routes/dalleRoutes.js
@@ -1,14 +1,12 @@
 import express from 'express'
 import { OPENAI_API_KEY } from '../config.js'
-import { Configuration, OpenAIApi } from 'openai'
+import OpenAI from 'openai'
 
 const router = express.Router()
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: OPENAI_API_KEY
 })
-
-const openai = new OpenAIApi(configuration)
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!!' })
@@ -18,20 +16,23 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
+      model: 'dall-e-2',
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
+      response_format: 'b64_json',
+      size: '1024x1024'
     })
 
-    const image = aiResponse.data.data[0].b64_json
-    res.status(200).json({ photo: image })
+    // const image = aiResponse.data.data[0].b64_json
+    console.log(aiResponse)
+
+    res.status(200).json({ photo: 'image' })
   } catch (error) {
     console.error(error)
     res
       .status(500)
-      .send(error?.response.data.error.message || 'Something went wrong')
+      .send(error.error.message || 'Something went wrong')
   }
 })
 
